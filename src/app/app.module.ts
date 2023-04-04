b6b91663836fbe61dc3f73c9436dcc18deeb6872
abhi16de <abhinandansharma3@cmail.carleton,ca>
2023-04-04 13:49:47 -0400

    added menu

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
